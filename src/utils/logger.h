59a78fc3cf18aee950d7393491223496f35a677a
Maschell <Maschell@gmx.de>
2022-04-22 22:16:49 +0200

    Control verbose logging via buildflags

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -11,8 +11,6 @@ extern "C" {
 #define __FILENAME_X__ (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
 #define __FILENAME__   (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILENAME_X__)
 
-// #define VERBOSE_DEBUG
-
 #ifdef DEBUG
 
 #ifdef VERBOSE_DEBUG

425fd82646bcc08f82ae6dd94de848733a3e3dae
Maschell <Maschell@gmx.de>
2022-04-22 22:14:37 +0200

    Always do error logging

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -34,6 +34,11 @@ extern "C" {
         WHBLogWritef("[%23s]%30s@L%04d: " FMT "", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
     } while (0)
 
+#define DEBUG_FUNCTION_LINE_ERR(FMT, ARGS...)                                                                \
+    do {                                                                                                     \
+        WHBLogPrintf("[%23s]%30s@L%04d: ##ERROR## " FMT "\n", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
+    } while (0)
+
 #else
 
 #define DEBUG_FUNCTION_LINE_VERBOSE(FMT, ARGS...) while (0)
@@ -42,12 +47,12 @@ extern "C" {
 
 #define DEBUG_FUNCTION_LINE_WRITE(FMT, ARGS...)   while (0)
 
-#endif
-
-#define DEBUG_FUNCTION_LINE_ERR(FMT, ARGS...)                                                                        \
-    do {                                                                                                             \
-        OSReport("## ERROR ## [%23s]%30s@L%04d: ##ERROR## " FMT "\n", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
+#define DEBUG_FUNCTION_LINE_ERR(FMT, ARGS...)                                                            \
+    do {                                                                                                 \
+        OSReport("[%23s]%30s@L%04d: ##ERROR## " FMT "\n", __FILENAME__, __FUNCTION__, __LINE__, ##ARGS); \
     } while (0)
+
+#endif
 
 void initLogging();
 
